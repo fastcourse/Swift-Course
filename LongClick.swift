6a8c7f38f01d0074bb33164a5833b1d974c0707a
fastcourse <81051586+fastcourse@users.noreply.github.com>
2021-03-23 22:11:37 +0100

    Update LongClick.swift

--- LongClick.swift
+++ LongClick.swift
@@ -1,1 +1,22 @@
+//https://stackoverflow.com/questions/30859203/uibutton-with-single-press-and-long-press-events-swift
+@IBOutlet weak var btn: UIButton!
 
+override func viewDidLoad() {
+
+    let tapGesture = UITapGestureRecognizer(target: self, #selector (tap))  //Tap function will call when user tap on button
+    let longGesture = UILongPressGestureRecognizer(target: self, #selector(long))  //Long function will call when user long press on button.
+    tapGesture.numberOfTapsRequired = 1
+    longGesture.minimumPressDuration = 2
+    btn.addGestureRecognizer(tapGesture)
+    btn.addGestureRecognizer(longGesture)
+}
+
+@objc func tap() {
+
+    print("Tap happend")
+}
+
+@objc func long() {
+
+    print("Long press")
+}
