396fdd0e31dcd99df125e164bf7edd55a6416fb1
fastcourse <81051586+fastcourse@users.noreply.github.com>
2021-03-24 15:46:28 +0100

    Update Label_Events.swift

--- Label_Events.swift
+++ Label_Events.swift
@@ -1,1 +1,19 @@
 
+// 1   we can with a trick create label OnClick :
+
+override func viewDidLoad() {
+        super.viewDidLoad()
+//label receiver un click
+        let tap = UITapGestureRecognizer(target: self, action: #selector(yourVC.labelReceiverClick))
+        labelReceiverUn.isUserInteractionEnabled = true
+        labelReceiverUn.addGestureRecognizer(tap)
+}
+    @objc func labelReceiverClick(sender:UITapGestureRecognizer) {
+       // view.endEditing(true)
+      print("label tapped")
+        //BtnSelectContact.sendActions(for: .touchUpInside)
+    }
+
+
+///////////////////////////////////////////////////////////////////
+// 2- :
