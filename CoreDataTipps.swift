88874845998abf0e3839c037294119c0206e467f
fastcourse <81051586+fastcourse@users.noreply.github.com>
2021-03-24 22:44:48 +0100

    Update CoreDataTipps.swift

--- CoreDataTipps.swift
+++ CoreDataTipps.swift
@@ -7,6 +7,6 @@ let contact_us = savedata1.string(forKey: "contact_us") ?? ""
 //ToSave
 UserDefaults.standard.set(self.contact_us, forKey: "contact_us")
 
-
+//example : https://github.com/Shashikant86/CoreDataDemo/blob/master/CoreDataDemo/ViewController.swift
 
 
