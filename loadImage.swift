8221b5d62a5886519e6559c699d42a2135f54465
fastcourse <81051586+fastcourse@users.noreply.github.com>
2021-04-12 23:22:04 +0200

    Update loadImage.swift

--- loadImage.swift
+++ loadImage.swift
@@ -23,39 +23,51 @@ override func viewDidLoad() {
         
         
   //load img into alert
-func Show_nafida_for_msgsOK4(x: Int){
+    func Show_nafida_for_msgs(x: Int){
+        let title1 = "Message!"
         let alert_msg = "text text text text text text text text text text text text text text text " +
         "text text text text text text text text text text text text text text text text text text1 "
-        let alert = UIAlertController(title: "Unfollow John Doe?", message: alert_msg, preferredStyle: .actionSheet)
+        let alert = UIAlertController(title: title1, message: alert_msg, preferredStyle: .actionSheet)
+
         let imageAction = UIAlertAction(title: "", style: .default, handler: nil)
         var image = UIImage(named: "Zurich")
-        //let imageView = UIImageView(frame: CGRect(origin: CGPoint(x: 0,y :0), size: CGSize(width: 196, height: 196)))
         let sender_img_path = self.contact_us+"siaha/msg_pictures/\(my_last_text_msg_private_img)"
-        //imageView.downloadedFrom(link: sender_img_path)  
+
         let url = URL(string: sender_img_path)!
+
             // Fetch Image Data
             if let data = try? Data(contentsOf: url) {
-                // Create Image and Update Image View
-                //imageView.image = UIImage(data: data)
                 image = UIImage(data: data)
-        }else{
-             image = UIImage(named: "galury_2_36")
+
         }
-                let alertViewPadding: CGFloat = 65.0 //Adjust this as per your need
+        
+        let msgFont = [NSAttributedStringKey.font: UIFont(name: "Avenir-Roman", size: 22.0)!]
+       // let titleFont = [NSAttributedStringKey.font: UIFont(name: "ArialHebrew-Bold", size: 22.0)!]
+        let msgAttrString = NSMutableAttributedString(string: title1, attributes: msgFont)
+        let titleAttrString = NSMutableAttributedString(string: alert_msg, attributes: msgFont)
+
+        let alertViewPadding: CGFloat = 65.0 //Adjust this as per your need
                 let left = -alert.view.frame.size.width / 2 + image!.size.width/2 + 20
                // let left = alert.view.frame.size.width/2 - image!.size.width/2
                 let centeredTopoImage = image?.withAlignmentRectInsets(UIEdgeInsetsMake(0, left, 0, 0)).withRenderingMode(.alwaysOriginal)
-                imageAction.setValue(centeredTopoImage, forKey: "image")  
-        let msgFont = [NSAttributedStringKey.font: UIFont(name: "Avenir-Roman", size: 22.0)!]
-        let msgAttrString = NSMutableAttributedString(string: alert_msg, attributes: msgFont)
-                /*let  deleteButton = UIAlertAction(title: "Unfollow", style: .destructive, handler: { (action) -> Void in
-                    print("Delete button tapped") })*/        
+                imageAction.setValue(centeredTopoImage, forKey: "image")
+
+
+                let  deleteButton = UIAlertAction(title: "Unfollow", style: .destructive, handler: { (action) -> Void in
+                    print("Delete button tapped")
+
+                })
+
                 let cancelButton = UIAlertAction(title: "Cancel", style: .cancel, handler: { (action) -> Void in
-                    print("Cancel button tapped")})          
-               alert.addAction(imageAction)
-                //alert.addAction(deleteButton)
+                    print("Cancel button tapped")
+
+                })
+
+                alert.addAction(imageAction)
+                alert.addAction(deleteButton)
                 alert.addAction(cancelButton)
                 self.present(alert, animated: true, completion: nil)
+                alert.setValue(titleAttrString, forKey: "attributedMessage")
                 alert.setValue(msgAttrString, forKey: "attributedMessage")
     }
 
