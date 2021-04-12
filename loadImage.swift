9b350236ba971b670b3a86c1ad592b593a3be644
fastcourse <81051586+fastcourse@users.noreply.github.com>
2021-04-12 17:21:42 +0200

    Update loadImage.swift

--- loadImage.swift
+++ loadImage.swift
@@ -1,11 +1,8 @@
 //1 : https://cocoacasts.com/fm-3-download-an-image-from-a-url-in-swift
 override func viewDidLoad() {
-    super.viewDidLoad()
-    
+    super.viewDidLoad()   
   //  loadImage1()
     loadImage2()
-
-    
     }
     
     func loadImage1(){
@@ -21,8 +18,5 @@ override func viewDidLoad() {
     let imageView = UIImageView(frame: CGRect(x: 10, y: 50, width: 250, height: 230))
     let sender_img_path = "https://cdn.cocoacasts.com/cc00ceb0c6bff0d536f25454d50223875d5c79f1/above-the-clouds.jpg"
     imageView.downloadedFrom(link: sender_img_path)  
-    }
-    
-    
-    
+    }    
 }
