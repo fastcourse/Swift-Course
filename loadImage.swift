2c134ed21daca2e204c9da5a521418330a3a7e85
fastcourse <81051586+fastcourse@users.noreply.github.com>
2021-04-12 17:10:39 +0200

    Update loadImage.swift

--- loadImage.swift
+++ loadImage.swift
@@ -1,1 +1,13 @@
+//1 : https://cocoacasts.com/fm-3-download-an-image-from-a-url-in-swift
+override func viewDidLoad() {
+    super.viewDidLoad()
 
+    // Create URL
+    let url = URL(string: "https://cdn.cocoacasts.com/cc00ceb0c6bff0d536f25454d50223875d5c79f1/above-the-clouds.jpg")!
+
+    // Fetch Image Data
+    if let data = try? Data(contentsOf: url) {
+        // Create Image and Update Image View
+        imageView.image = UIImage(data: data)
+    }
+}
