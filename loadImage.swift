0c156df1f82b7915acc8df446d90133ba83b7423
fastcourse <81051586+fastcourse@users.noreply.github.com>
2021-04-15 15:00:58 +0200

    Update loadImage.swift

--- loadImage.swift
+++ loadImage.swift
@@ -198,6 +198,49 @@ func Show_nafida_for_msgs(x: Int){
         
     }
         
+        1- ActionSheet as Alert with image and format title and msg
+  //load img into alert
+    func Show_nafida_for_msgs(x: Int){
+        let title1 = "Message!"
+        let alert_msg = "text text text text text text text text text text text text text text text " +
+        "text text text text text text text text text text text text text text text text text text1 "
+        let alert = UIAlertController(title: title1, message: alert_msg, preferredStyle: .actionSheet)
+        let imageAction = UIAlertAction(title: "", style: .default, handler: nil)
+        var image = UIImage(named: "Zurich")
+        let sender_img_path = self.contact_us+"siaha/msg_pictures/\(my_last_text_msg_private_img)"
+        let url = URL(string: sender_img_path)!
+           // Fetch Image Data
+            if let data = try? Data(contentsOf: url) {
+                image = UIImage(data: data)
+        }    
+        let msgFont = [NSAttributedStringKey.font: UIFont(name: "Avenir-Roman", size: 22.0)!]
+        let titleFont = [NSAttributedStringKey.font: UIFont(name: "ArialHebrew-Bold", size: 22.0)!]
+        //fontnames : https://github.com/lionhylra/iOS-UIFont-Names
+        let msgAttrString = NSMutableAttributedString(string: alert_msg, attributes: msgFont)
+        let titleAttrString = NSMutableAttributedString(string: title1, attributes: titleFont)
+        let alertViewPadding: CGFloat = 65.0 //Adjust this as per your need
+                let left = -alert.view.frame.size.width / 2 + image!.size.width/2 + 20
+               // let left = alert.view.frame.size.width/2 - image!.size.width/2
+                let centeredTopoImage = image?.withAlignmentRectInsets(UIEdgeInsetsMake(0, left, 0, 0)).withRenderingMode(.alwaysOriginal)
+                imageAction.setValue(centeredTopoImage, forKey: "image")
+                let  deleteButton = UIAlertAction(title: "Unfollow", style: .destructive, handler: { (action) -> Void in
+                    print("Delete button tapped")
+                })
+                let cancelButton = UIAlertAction(title: "Cancel", style: .cancel, handler: { (action) -> Void in
+                    print("Cancel button tapped")
+                })
+                alert.addAction(imageAction)
+                alert.addAction(deleteButton)
+                alert.addAction(cancelButton)
+                self.present(alert, animated: true, completion: nil)
+                alert.setValue(titleAttrString, forKey: "attributedTitle")
+                alert.setValue(msgAttrString, forKey: "attributedMessage")
+        
+               alert.view.tintColor = UIColor.black
+               alert.view.backgroundColor = UIColor.blue
+               alert.view.layer.cornerRadius = 40
+    } 
+        
         
         
         
