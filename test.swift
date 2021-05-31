96454f5edb96359983b0d14bd5a5c9d4c684d4ae
fastcourse <81051586+fastcourse@users.noreply.github.com>
2021-05-31 19:15:35 +0200

    Update test.swift

--- test.swift
+++ test.swift
@@ -1,1 +1,13 @@
 
+URLSession.shared.dataTask(with: serverUrl!) { [self] (data, res, error) in
+                do {
+                    self.myMediaPlayer1 = try AVAudioPlayer(data: data!)
+                    self.myMediaPlayer1.prepareToPlay()
+                    self.myMediaPlayer1.delegate = self
+                    self.SetSessionPlayerOn()
+                    self.loading_img.isHidden = true    // not work til audio finish playing  
+                    self.myMediaPlayer1.play()
+                }catch let error {
+                    print("error!!!!!!!!!!")
+                }
+            }.resume()
