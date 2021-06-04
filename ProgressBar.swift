281874f6c3e10af893f9c0a2d13302da1b555a2d
fastcourse <81051586+fastcourse@users.noreply.github.com>
2021-06-04 12:33:59 +0200

    Update ProgressBar.swift

--- ProgressBar.swift
+++ ProgressBar.swift
@@ -1,1 +1,29 @@
 
+////////    1 : default one : but this can not be canceled    /////////////
+
+let hud = MBProgressHUD.showAdded(to: self.view, animated: true)
+hud.label.text = "Sending . . ."
+// to hide it
+hud.isHidden = true
+
+
+
+////////    2 : cancelabe Progressbar by click on it.     ///////////////
+var hud = MBProgressHUD()
+// to show it :
+showHUD(withCancel: "Sending . . .")
+// to hide it :
+hud.isHidden = true
+
+
+    func showHUD(withCancel aMessage: String?) {
+        hud = MBProgressHUD.showAdded(to: view, animated: true)
+        hud.label.text = aMessage
+        hud.detailsLabel.text = "Stop"
+      //click event on it to hide it or to try again :
+        hud.addGestureRecognizer(UITapGestureRecognizer(target: self, action: #selector(hideHudNow))) 
+    }
+    
+    @objc func hideHudNow() {
+        hud.isHidden = true
+    }
